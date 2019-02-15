82e7eaf8835512fbbbb942166645d0b555782ffc
Dennis Houwen <18077943@student.hhs.nl>
2019-02-15 12:37:37 +0100

    JSON inlezen en deserializen toegevoegd

--- Hotel/Program.cs
+++ Hotel/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Hotel
 {
@@ -17,6 +19,9 @@ namespace Hotel
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            string json = File.ReadAllText(@"d:\hotel.layout"); // Leest de file
+            List<Rooms> kamers = JsonConvert.DeserializeObject<List<Rooms>>(json); // zet de JSON om in een lijst in Rooms.cs
         }
     }
 }
---
+++ Hotel/Rooms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    /// <summary>
+    /// Hier staan de attributen van de kamers
+    /// </summary>
+    public class Rootobject
+    {
+        public Rooms[] Property1 { get; set; }
+    }
+
+    public class Rooms
+    {
+        public string AreaType { get; set; }
+        public string Position { get; set; }
+        public string Dimention { get; set; }
+        public int Capacity { get; set; }
+        public string Classification { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+    }
+
+}
